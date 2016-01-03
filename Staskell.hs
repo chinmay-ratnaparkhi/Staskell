66f1cd2e1119addf79e8864e2b09fdb6391f37d9
c732r120 <chinmay.ratnaparkhi@gmail.com>
2016-01-03 16:03:53 -0600

    The pop method and instance show
    The pop method accepts a stack and returns a new one after eliminating
    the top element. An instance of the Show class is defined which prints
    the stack.

--- Staskell.hs
+++ Staskell.hs
@@ -1,19 +1,28 @@
-data Stack = Empty | Full Double Stack deriving (Show)
+data Stack = Empty | Full Double Stack --deriving (Show)
 
 
--- The insert method accepts a double, a stack and returns a new stack
+-- The insert function accepts a double, a stack and returns a new stack
 -- with the value added to the top of the stack.
 insert :: Double -> Stack -> Stack
 insert x y = Full x y
 
--- The inserter method takes a list and builds a stack with everything in
--- the list
-inserter :: [Double] -> Stack
-inserter xs =  foldl (\acc a -> (insert a acc)) Empty xs
-
-
--- The peek method takes a stack and returns the value that's at the 
+-- The peek function takes a stack and returns the value that's at the 
 -- top of the stack.
 peek :: Stack -> Maybe Double
 peek Empty = Nothing
 peek (Full x _) = Just x
+
+-- The pop function takes a stack and returns a new stack after eleminating
+-- the element at the top.
+pop :: Stack -> Stack
+pop Empty = Empty
+pop (Full _ y) = y
+
+-- The inserter function takes a list and builds a stack with everything in
+-- the list
+inserter :: [Double] -> Stack
+inserter xs =  foldl (\acc a -> (insert a acc)) Empty xs
+
+instance Show Stack where
+	show Empty = "Empty \n--------------"
+	show (Full x y) = (show x) ++ "\n" ++ show y
